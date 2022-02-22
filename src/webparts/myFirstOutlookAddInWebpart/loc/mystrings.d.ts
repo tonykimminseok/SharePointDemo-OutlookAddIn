813b9e6efe960df155a98b38abf1f4d07864d8d8
Tony Minseok Kim <tony.minseokkim@tectiqom.de>
2022-02-22 10:33:20 -0800

    first commit

---
+++ src/webparts/myFirstOutlookAddInWebpart/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IMyFirstOutlookAddInWebpartWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'MyFirstOutlookAddInWebpartWebPartStrings' {
+  const strings: IMyFirstOutlookAddInWebpartWebPartStrings;
+  export = strings;
+}
